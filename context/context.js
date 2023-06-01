214cf722021bc934e6b2816c65888a4a056f0351
sutarrohit <rohitsutar082@gmail.com>
2023-06-01 22:15:53 +0530

    update

--- context/context.js
+++ context/context.js
@@ -3,5 +3,5 @@ import ERC20ABI from "./ERC20ABI.json";
 
 export const contractABI = ABI;
 export const contractERC20ABI = ERC20ABI;
-export const contractAddress = "0x25351FBBc947dc3eC43e8474797E898b2981c081";
+export const contractAddress = "0xB7b192E3371eD40B5C338DA8a8e5c42542C64A86";
 export const TokenContractAddress = "0x53bd0c51A6052f508C029F4970BE63ea271dA724";
